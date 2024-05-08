93916ec4b2257c8af3aa4b2f127a81e296ee827b
Alex Bustamante <alex@Alexs-iMac.local>
2024-05-08 14:46:36 -0700

    Updated project structure and layout

---
+++ main.js
@@ -0,0 +1,119 @@
+ let myArray = ["3","9", "23", "64", "2","8","28","93"]; //1
+myArray.Slice(0,7, '3', '93');
+console.log(myArray);
+
+let ages = ["3","9", "23", "64", "2","8","28","93"];
+myArray.push("17");
+for (let i = 0; i < ages.length; i++) {
+    console.log(ages[i]);
+}
+
+let names = ["Sam", "Tommy", "Tim","Sally","Buck","Bob"]; //2
+for (let i = 0; i < ages.length; i++) {
+    console.log(Names[i]);
+
+}
+for (let i = 0; i < ages.length; i++) {
+    console.log(names.Together[i]);
+
+    // you access the last element of an array,  by using the  pop(): array //3
+    // you access the first element of an array, by using the array.slice () method //4
+
+   
+    let nameLengths = ["Sam", "Tommy", "Tim","Sally","Buck","Bob"]; //5
+    let nameLengths = [3,5,3,5,4,3];
+    
+    for (let i = 0; i < name.Lengths i++) {
+        console.log(previous.names[i]); // 6 i need help on this one 
+
+
+        var numbers = [3,5,3,5,4,3];    
+
+var sum = numbers.reduce(function(total, currentNumber) {
+    return total + currentNumber;
+}, 0);
+
+console.log(sum);
+
+
+function pere(word, n) {        // 7
+    return word * n;
+
+}
+
+console.log(pere("hello", 10));
+
+
+
+
+
+function pere(word, n) {
+    return word * n;
+
+}
+
+console.log(pere("hello",)(10));
+
+function pere(word, n) {
+   console.log(word * n);
+
+}
+
+ pere("hello", 10));
+
+
+//8
+// write code below this line
+function fullName(first, last){
+    var full = 
+    return full;
+ }
+
+ 
+
+ var firstName = "Alex";
+ var lastName = "Bustamante";
+ 
+ console.log( fullName(firstName, lastName) ); 
+ 
+ var user1 = fullName( "Alex");
+ console.log( user1 ); 
+
+//9
+if(a + b < 100){
+    return true;
+}else{
+    return false;
+}
+
+//10
+
+let sum = 2;
+for(let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+const average = sum / numbers.length;
+
+//11.
+
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(let i = 0; i <numbers.length; i++){
+    return true;
+}else{ 
+    return false;
+}
+//12
+function willBuyDrink = (isHotOutside, moneyInPocket) => {
+    let willBuyDrink = isHotOutside
+    return moneyInPocket => 10.50;
+    return true;
+}
+//13
+function myFucntion(willDoPushup, RepsperWeek) => {
+    let weekly = monthly / 30;
+    return monthly.toFixed(2);
+} 
+// this function is attempting to return monthly pushups reps per week I  just need more help inputting reps per week 
